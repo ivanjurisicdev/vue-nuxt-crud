66a396ff1f91779f64b16c641555d8e61a2bb4e3
Ivan Jurisic <ivan@iconisagency.com>
2020-08-11 15:18:43 +0200

    Changes products panel style

--- components/Products.vue
+++ components/Products.vue
@@ -10,19 +10,111 @@
       <div v-if="loading">Loading...</div>
 
       <div v-else>
+        <v-toolbar flat>
+          <div v-for="item in items" :key="item.id" class="item">
+            <v-toolbar-title>{{ item.name }}</v-toolbar-title>
+          </div>
+
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on"
+                >New Item</v-btn
+              >
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">New Item</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="productItems.title"
+                        label="Title"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="productItems.description"
+                        label="Description"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="productItems.price"
+                        label="Price"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="productItems.category"
+                        label="Category"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="productItems.employee"
+                        label="Employee"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+
         <v-expansion-panels>
           <v-expansion-panel v-for="product in products" :key="product.id">
-            <v-expansion-panel-header>{{
-              product.data.title
-            }}</v-expansion-panel-header>
+            <v-expansion-panel-header>
+              {{ product.data.title }}</v-expansion-panel-header
+            >
             <v-expansion-panel-content>
+              <br />
+              {{ product.data.category }}
+              <br />
+              {{ product.data.price }}
+              <br />
+              {{ product.data.employee }}
+              <br />
               {{ product.data.description }}
+              <br />
               <v-icon small @click="deleteItem(product)">
                 mdi-delete
               </v-icon>
+              <router-link
+                :to="{
+                  name: 'details',
+                  params: {
+                    id: product.id,
+                    title: product.data.title,
+                    description: product.data.description,
+                    price: product.data.price,
+                    category: product.data.category,
+                  },
+                }"
+              >
+                <br />
+                Link
+                <v-icon small class="mr-2">
+                  mdi-eye-outline
+                </v-icon>
+              </router-link>
             </v-expansion-panel-content>
           </v-expansion-panel>
         </v-expansion-panels>
+
+        <br /><br /><br />
 
         <v-data-table
           :headers="headers"
